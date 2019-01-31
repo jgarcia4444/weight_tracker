b2ca480d070339d9bb6ec922461846b1cec1f853
Jake Garcia <bballwiz4@gmail.com>
2019-01-30 16:05:21 -0800

    name and weight input added addWeight function added if the name matches Jake it adds the weight to the object associated with the user Jake

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,25 @@
 <template>
-  <div id="app" class="container-fluid text-center">
+  <div id="app" class="text-center">
+    <!--nav bar showing the name of the web app clicking the name redirects to same page-->
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
       <a class="navbar-brand" href="/">Jake's Weight Tracker</a>
     </nav>
-    <router-view></router-view>
+    <div class="container mt-5">
+      <form @submit.prevent="addWeight()">
+        <div class="form-group row">
+          <div class="col-sm-6">
+            <label for="name">Name:</label>
+            <input v-model="inputName" class="form-control" type="text" id="name" required/>
+          </div>
+          <div class="col-sm-6">
+            <label for="weight">Weight</label>
+            <input v-model="newWeight" class="form-control" type="number" id="weight" required/>
+            <small id="weight">(lbs)</small>
+          </div>
+        </div>
+        <button class="btn btn-primary" type="submit">Add weight</button>
+      </form>
+    </div>
   </div>
 </template>
 
@@ -12,11 +28,30 @@
 
 export default {
   name: 'app',
-  data: {
-
+  data() {
+    return {
+      // users array, newWeight, 
+      users: [
+        {user: 'Jake', weight: [],},
+        // {name: 'Andrea', weight: [],}
+      ],
+      newWeight: 0,
+      inputName: '',
+    }
+  },
+  methods: {
+    // push the user name with new weight input into the users array as an // object
+    addWeight() {
+      if (this.inputName == 'Jake') {
+        this.users[0].weight.push(this.newWeight);
+      } else {
+        console.log('Hello World')
+      }
+    },
   }
 };
 </script>
 
 <style>
+
 </style>
